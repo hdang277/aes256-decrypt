472c71f536d2e2ee618f4ddfbfd829f3c4c5ad99
hdang277 <117496965+hdang277@users.noreply.github.com>
2023-07-04 14:52:25 +0700

    Update tb_main.vhd

--- tb_main.vhd
+++ tb_main.vhd
@@ -9,11 +9,11 @@ end tb_main;
 architecture Behavioral of tb_main is
 component main is 
     port(
-    unencrypted   : in std_logic_vector(127 downto 0) ;
+    ciphertext   : in std_logic_vector(127 downto 0) ;
     key           : in std_logic_vector(255 downto 0) ;
     clk           : in std_logic;
 
-    crypted       : out std_logic_vector(127 downto 0)
+    plaintext       : out std_logic_vector(127 downto 0)
 
     
     
@@ -22,21 +22,21 @@ component main is
     
     
 end component; 
-signal    unencrypted   : std_logic_vector(127 downto 0) ;
+signal    ciphertext   : std_logic_vector(127 downto 0) ;
 signal    key           : std_logic_vector(255 downto 0) ;
 signal    clk           : std_logic;
 
-signal    crypted       :  std_logic_vector(127 downto 0);
+signal    plaintext       :  std_logic_vector(127 downto 0);
 constant clock_period   : time := 8ns;
 
 begin
 
 uut: main port map( 
-                unencrypted => unencrypted,
+                ciphertext => ciphertext,
                 key => key,
                 clk => clk,
 
-                crypted => crypted );
+                plaintext => plaintext );
 
 clock_Process: process
     begin
@@ -50,14 +50,14 @@ clock_Process: process
 stim_proc: process
     begin              
         
-        unencrypted <= x"8ea2b7ca516745bfeafc49904b496089"; -- example 1
-        --unencrypted <= x"1546467a7687b768d97979c6687f987a"; -- example  2
-        --unencrypted <= x"e787897f989789c879879b78787897f2";  -- example  3
+        --unencrypted <= x"8ea2b7ca516745bfeafc49904b496089"; -- example 1
+        ciphertext <= x"f66f84a7e68b339ae78d5e61c9495c8a"; -- example  2
+        
                      
     
          key         <= x"000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f";  -- example 1 
         -- key         <= x"43a564b5634e55634f6534e32324f132" ; -- example  2
-        -- key         <= x"ad2387b5e4f678435813798132aed6c5" ; -- example  3
+        
         
     wait for clock_period*30;
        wait;  
