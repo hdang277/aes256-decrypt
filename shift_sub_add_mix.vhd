9068bcdceb6a8424dbbfbf8b15564cc233258a11
hdang277 <117496965+hdang277@users.noreply.github.com>
2023-07-04 14:50:45 +0700

    Update shift_sub_add_mix.vhd

--- shift_sub_add_mix.vhd
+++ shift_sub_add_mix.vhd
@@ -75,14 +75,14 @@ TYPE sBoxArray IS ARRAY (NATURAL RANGE 0 TO 255) OF STD_LOGIC_VECTOR(7 DOWNTO 0)
         x"7c", x"e3", x"39", x"82", x"9b", x"2f", x"ff", x"87", x"34", x"8e", x"43", x"44", x"c4", x"de", x"e9", x"cb",
         x"54", x"7b", x"94", x"32", x"a6", x"c2", x"23", x"3d", x"ee", x"4c", x"95", x"0b", x"42", x"fa", x"c3", x"4e",
         x"08", x"2e", x"a1", x"66", x"28", x"d9", x"24", x"b2", x"76", x"5b", x"a2", x"49", x"6d", x"8b", x"d1", x"25",
-        x"72", x"78", x"f6", x"64", x"86", x"68", x"98", x"16", x"d4", x"a4", x"5c", x"cc", x"5d", x"65", x"b6", x"92",
+        x"72", x"f8", x"f6", x"64", x"86", x"68", x"98", x"16", x"d4", x"a4", x"5c", x"cc", x"5d", x"65", x"b6", x"92",
         x"6c", x"70", x"48", x"50", x"fd", x"ed", x"b9", x"da", x"5e", x"15", x"46", x"57", x"a7", x"8d", x"9d", x"84",
         x"90", x"d8", x"ab", x"00", x"8c", x"bc", x"d3", x"0a", x"f7", x"e4", x"58", x"05", x"b8", x"b3", x"45", x"06",
         x"d0", x"2c", x"1e", x"8f", x"ca", x"3f", x"0f", x"02", x"c1", x"af", x"bd", x"03", x"01", x"13", x"8a", x"6b",
         x"3a", x"91", x"11", x"41", x"4f", x"67", x"dc", x"ea", x"97", x"f2", x"cf", x"ce", x"f0", x"b4", x"e6", x"73",
         x"96", x"ac", x"74", x"22", x"e7", x"ad", x"35", x"85", x"e2", x"f9", x"37", x"e8", x"1c", x"75", x"df", x"6e",
         x"47", x"f1", x"1a", x"71", x"1d", x"29", x"c5", x"89", x"6f", x"b7", x"62", x"0e", x"aa", x"18", x"be", x"1b",
-        x"fc", x"56", x"3e", x"4b", x"c6", x"dc", x"79", x"20", x"9a", x"db", x"c0", x"fe", x"78", x"cd", x"5a", x"f4",
+        x"fc", x"56", x"3e", x"4b", x"c6", x"d2", x"79", x"20", x"9a", x"db", x"c0", x"fe", x"78", x"cd", x"5a", x"f4",
         x"1f", x"dd", x"a8", x"33", x"88", x"07", x"c7", x"31", x"b1", x"12", x"10", x"59", x"27", x"80", x"ec", x"5f",
         x"60", x"51", x"7f", x"a9", x"19", x"b5", x"4a", x"0d", x"2d", x"e5", x"7a", x"9f", x"93", x"c9", x"9c", x"ef",
         x"a0", x"e0", x"3b", x"4d", x"ae", x"2a", x"f5", x"b0", x"c8", x"eb", x"bb", x"3c", x"83", x"53", x"99", x"61",
@@ -489,7 +489,7 @@ begin
     e2_mul_4(6) := e2_mul_2(5);
     e2_mul_4(7) := e2_mul_2(6);
 
-    -- 2*a3
+    -- 4*a3
     a3_mul_4(0) := a3_mul_2(7);
     a3_mul_4(1) := a3_mul_2(0) xor a3_mul_2(7);
     a3_mul_4(2) := a3_mul_2(1);
@@ -497,7 +497,7 @@ begin
     a3_mul_4(4) := a3_mul_2(3) xor a3_mul_2(7);
     a3_mul_4(5) := a3_mul_2(4);
     a3_mul_4(6) := a3_mul_2(5);
-    a3_mul_4(7) :=a3_mul_2(6);
+    a3_mul_4(7) := a3_mul_2(6);
 	
 	q3_mul_4(0) := q3_mul_2(7);
     q3_mul_4(1) := q3_mul_2(0) xor q3_mul_2(7);
@@ -624,7 +624,7 @@ begin
     w2_mul_8(1) := w2_mul_4(0) xor w2_mul_4(7);
     w2_mul_8(2) := w2_mul_4(1);
     w2_mul_8(3) := w2_mul_4(2) xor w2_mul_4(7);
-    w2_mul_8(4) := w2_mul_4(3) xor w2_mul_2(7);
+    w2_mul_8(4) := w2_mul_4(3) xor w2_mul_4(7);
     w2_mul_8(5) := w2_mul_4(4);
     w2_mul_8(6) := w2_mul_4(5);
     w2_mul_8(7) := w2_mul_4(6);
@@ -646,7 +646,7 @@ begin
     a3_mul_8(4) := a3_mul_4(3) xor a3_mul_4(7);
     a3_mul_8(5) := a3_mul_4(4);
     a3_mul_8(6) := a3_mul_4(5);
-    a3_mul_8(7) :=a3_mul_4(6);
+    a3_mul_8(7) := a3_mul_4(6);
 	
 	q3_mul_8(0) := q3_mul_4(7);
     q3_mul_8(1) := q3_mul_4(0) xor q3_mul_4(7);
@@ -706,7 +706,7 @@ begin
     
     
     end process mix_columns_p;
-    output <= tmp_2 when mix_counter = "1111"  else
+    output <= tmp_2 when mix_counter = "1110"  else
                         B0xDO & B1xDO & B2xDO & B3xDO & Q0xDO & Q1xDO & Q2xDO & Q3xDO & W0xDO & W1xDO & W2xDO & W3xDO & E0xDO & E1xDO & E2xDO & E3xDO;
 	
 	     
